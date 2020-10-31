994fd738bf4e28db663567665795e8261ec2a5af
Yousuf Kalim <yousufkalim@outlook.com>
2020-10-31 14:02:42 +0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        responsiveClass:true,
+        nav:false,
+        dots:true,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    })
+  });
